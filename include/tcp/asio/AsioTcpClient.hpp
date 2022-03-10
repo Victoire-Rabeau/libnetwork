b8e5c4b95283cee09f509665f6d1197054f0e62f
Victoire RABEAU <victoire.rabeau@epitech.eu>
2022-03-10 17:19:57 +0100

    feat(tcp/asio): Add AsioTcpListener and AsioTcpListener classes

--- include/tcp/asio/AsioTcpClient.hpp
+++ include/tcp/asio/AsioTcpClient.hpp
@@ -1,8 +1,38 @@
-//
-// Created by victoire on 3/10/22.
-//
-
 #ifndef ASIOTCPCLIENT_HPP
 #define ASIOTCPCLIENT_HPP
+
+#include "tcp/ITcpClient.hpp"
+
+namespace network::tcp
+{
+    template <typename Request, typename Response>
+    class AsioTcpClient : public ITcpClient<Request, Response>
+    {
+        public:
+
+            AsioTcpClient(std::uint16_t port)
+            {}
+
+            ~AsioTcpClient() override = default;
+
+            void connect(Address const &peer) override
+            {}
+
+            void disconnect() override
+            {}
+
+            [[nodiscard]] std::size_t send(Response const &packet) override
+            {}
+
+            [[nodiscard]] Request receive() override
+            {}
+
+            void asyncSend(Response const &packet, std::function<void (int)> &&callback) noexcept override
+            {}
+
+            virtual void asyncReceive(std::function<void (int, Request &)> &&callback) noexcept override
+            {}
+    };
+}
 
 #endif //ASIOTCPCLIENT_HPP
--- include/tcp/asio/AsioTcpListener.hpp
+++ include/tcp/asio/AsioTcpListener.hpp
@@ -1,8 +1,26 @@
-//
-// Created by victoire on 3/10/22.
-//
-
 #ifndef ASIOTCPLISTENER_HPP
 #define ASIOTCPLISTENER_HPP
+
+#include "tcp/ITcpListener.hpp"
+
+namespace network::tcp
+{
+    template <typename Request, typename Response>
+    class AsioTcpListener : public ITcpListener<Request, Response>
+    {
+        public:
+
+            AsioTcpListener(std::uint16_t port)
+            {}
+
+            ~AsioTcpListener() override = default;
+
+            void run(std::function<void (int, std::unique_ptr<network::tcp::ITcpClient<Request, Response>>)> &&callback) noexcept override
+            {}
+
+            void stop() noexcept override
+            {}
+    };
+}
 
 #endif //ASIOTCPLISTENER_HPP
