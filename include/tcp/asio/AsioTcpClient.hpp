3de7a05499bd42914eb77e999794ec3ab5b72d7f
Victoire RABEAU <victoire.rabeau@epitech.eu>
2022-03-24 22:18:54 +0100

    feat(tcp): Add synchronous and asynchronous send and receive methods in AsioTcpClient

--- include/tcp/asio/AsioTcpClient.hpp
+++ include/tcp/asio/AsioTcpClient.hpp
@@ -1,6 +1,10 @@
 #ifndef ASIOTCPCLIENT_HPP
 #define ASIOTCPCLIENT_HPP
 
+#include <asio/ip/tcp.hpp>
+#include <asio/read.hpp>
+#include <asio/write.hpp>
+
 #include "tcp/ITcpClient.hpp"
 
 namespace network::tcp
@@ -10,28 +14,79 @@ namespace network::tcp
     {
         public:
 
-            AsioTcpClient(std::uint16_t port)
+            AsioTcpClient() :
+                _ioContext{},
+                _tcpSocket{_ioContext},
+                _run{false}
             {}
 
-            ~AsioTcpClient() override = default;
+            ~AsioTcpClient() override
+            {
+                _ioContext.stop();
+            }
 
             void connect(Address const &peer) override
-            {}
+            {
+                asio::ip::tcp::endpoint endpoint{asio::ip::address{peer.ip}, peer.port};
+
+                _tcpSocket.connect(endpoint);
+                _run = true;
+            }
 
             void disconnect() override
-            {}
+            {
+                _run = false;
+            }
 
             [[nodiscard]] std::size_t send(Response const &packet) override
-            {}
+            {
+                std::size_t writeSize;
+
+                try {
+                    asio::write(_tcpSocket, asio::buffer(&packet, sizeof(Response)));
+                } catch (asio::system_error const &e) {
+                    throw std::runtime_error{"ERROR(network/tcp/asio/AsioTcpClient): Unable to send data: " + std::string{e.what()}};
+                }
+                return writeSize;
+            }
 
             [[nodiscard]] Request receive() override
-            {}
+            {
+                Request request;
+
+                try {
+                    asio::read(_tcpSocket, asio::buffer(request, sizeof(request)));
+                } catch (asio::system_error const &e) {
+                    throw (std::runtime_error{"ERROR(network/tcp/asio/AsioTcpClient): Unable to receive data: " + std::string{e.what()}});
+                }
+                return request;
+            }
 
             void asyncSend(Response const &packet, std::function<void (int)> &&callback) noexcept override
-            {}
+            {
+                asio::async_write(_tcpSocket, asio::buffer(&packet, sizeof(Response)),
+                                  [callback = std::move(callback)] (int errorCode,std::size_t) {
+                        callback(errorCode);
+                });
+            }
 
-            virtual void asyncReceive(std::function<void (int, Request &)> &&callback) noexcept override
-            {}
+            void asyncReceive(std::function<void (int, Request &)> &&callback) noexcept override
+            {
+                Request request;
+
+                asio::async_read(_tcpSocket, asio::buffer(&request, sizeof(Request)),
+                                 [this, callback = std::move(callback), request] (int errorCode, std::size_t) mutable {
+                    callback(errorCode, request);
+                    if (!_run)
+                        return;
+                    asyncReceive(std::move(callback));
+                });
+            }
+
+        private:
+            asio::io_context _ioContext;
+            asio::ip::tcp::socket _tcpSocket;
+            bool _run;
     };
 }
 
